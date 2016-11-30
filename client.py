6dca40b994a2116e402d323be42794893b7d9d58
PierreChauveau <piechauveau@emi.u-bordeaux1.fr>
2016-11-30 18:44:25 +0100

    Avancement du projet

--- client.py
+++ client.py
@@ -2,6 +2,46 @@
 
 from grid import *
 import socket
+import os, sys
 
 def main():
+    serv = sys.argv[1]
+    print serv
+    connect = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    connect.connect((serv, 6666))
+
+    gameLaunched = connect.recv(1024)
+    gameLaunched = gameLaunched.decode("ascii")
+    print gameLaunched
+
+
+
+    end = False
     
+    while(not end):
+        whatToDo = connect.recv(1024)
+        whatToDo = whatToDo.decode("ascii")
+        if whatToDo == "c":
+            shot = int(input ("Quelle case voulez-vous jouer ?"))
+            shot = str(shot)
+            shot = shot.encode("ascii")
+            connect.send(shot)
+        if whatToDo == "w":
+            print "Vous avez gagne !"
+            end = True
+        if whatToDo == "l":
+            print "Vous avez perdu."
+            end = True
+            
+
+    
+    
+
+
+
+
+
+
+
+    connect.close()
+main()
