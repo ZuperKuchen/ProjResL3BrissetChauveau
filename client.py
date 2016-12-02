37580f5df933f80173e8dd2a9c7538a17a87206c
PierreChauveau <piechauveau@emi.u-bordeaux1.fr>
2016-12-02 15:33:27 +0100

    Avancement

--- client.py
+++ client.py
@@ -10,15 +10,19 @@ def main():
     connect = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     connect.connect((serv, 6666))
 
-    gameLaunched = connect.recv(1024)
-    gameLaunched = gameLaunched.decode("ascii")
-    print gameLaunched
+    player = connect.recv(1024)
+    player = player.decode("ascii")
+    if player == "J1":
+        player = J1
+    else :
+        player = J2
+    
+    print "Les deux joueurs sont connectes, le jeu commence."
 
-
-
+    playerGrid = grid()
     end = False
     
-    while(not end):
+    while not end:
         whatToDo = connect.recv(1024)
         whatToDo = whatToDo.decode("ascii")
         if whatToDo == "c":
@@ -26,6 +30,18 @@ def main():
             shot = str(shot)
             shot = shot.encode("ascii")
             connect.send(shot)
+
+            shot = int(shot)
+            answer = connect.recv(1024)
+            answer.decode("ascii")
+            if answer == "f":
+                playerGrid.play(player, shot)
+            else :
+                if playerGrid.cells[shot] == EMPTY :
+                    playerGrid.play(player%2+1, shot)
+            
+            playerGrid.display()
+            
         if whatToDo == "w":
             print "Vous avez gagne !"
             end = True
