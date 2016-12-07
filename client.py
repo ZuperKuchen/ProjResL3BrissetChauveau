a6e0aadd8a941393b7fa3d97eb680840a86611f8
PierreChauveau <piechauveau@emi.u-bordeaux1.fr>
2016-12-07 15:10:35 +0100

    Finalisation jeu base

--- client.py
+++ client.py
@@ -4,20 +4,33 @@ from grid import *
 import socket
 import os, sys
 
+def recep_info(grid, connect):
+    for i in range 9:
+        recept = connect.recv(1024)
+        if recept == 1:
+            grid.cells[i] = 1
+        elif recept == 2:
+            grid.cells[i] = 2
+        else:
+            grid.cells[i] = EMPTY
+
+    
+
 def main():
     serv = sys.argv[1]
-    print serv
+    print(serv)
     connect = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     connect.connect((serv, 6666))
 
     player = connect.recv(1024)
     player = player.decode("ascii")
-    if player == "J1":
-        player = J1
+    print(player)
+    if player == "1":
+        player = 1
     else :
-        player = J2
+        player = 2
     
-    print "Les deux joueurs sont connectes, le jeu commence."
+    print("Les deux joueurs sont connectes, le jeu commence.")
 
     playerGrid = grid()
     end = False
@@ -25,6 +38,7 @@ def main():
     while not end:
         whatToDo = connect.recv(1024)
         whatToDo = whatToDo.decode("ascii")
+        print(whatToDo)
         if whatToDo == "c":
             shot = int(input ("Quelle case voulez-vous jouer ? "))
             shot = str(shot)
@@ -34,19 +48,23 @@ def main():
             shot = int(shot)
             answer = connect.recv(1024)
             answer.decode("ascii")
-            if answer == "f":
+            print(answer)
+            if answer == b'f':
                 playerGrid.play(player, shot)
             else :
                 if playerGrid.cells[shot] == EMPTY :
-                    playerGrid.play(player%2+1, shot)
+                    if player == 1:
+                        playerGrid.play(2, shot)
+                    else:
+                        playerGrid.play(1, shot)
             
             playerGrid.display()
             
         if whatToDo == "w":
-            print "Vous avez gagne !"
+            print("Vous avez gagne !")
             end = True
         if whatToDo == "l":
-            print "Vous avez perdu."
+            print("Vous avez perdu.")
             end = True
             
 
